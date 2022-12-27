dc17428d7107c8bf553394bbe7e9d4e37e919d5f
Paula Vieira <eipvi@proton.me>
2022-12-26 22:40:19 -0300

    menu ajust basics

--- js/main.js
+++ js/main.js
@@ -1,6 +1,29 @@
 //declaring variables
+var toggleMenu = document.querySelectorAll('.jl-toggle-menu');
+var menuMobile = document.querySelector('.jl-menu-mob');
 
+//page Preloader
 
 
 //opening and closing menu mobile
 
+
+//abrindo e fechando menu mobile
+
+for (var m = 0; m < toggleMenu.length; m++) {
+    toggleMenu[m].addEventListenner('click', function () {
+        var overlay = document.querySelector('.jl-menu-overlay');
+        menuMobile.classList.toggle('jl-menu-is-closed');
+        menuMobile.classList.toggle('jl-menu-is-open');
+    });
+}
+
+//abrindo e fechando modal O.
+for (var i = 0; i < toggleModal.length; i++) {
+    toggleMenu[i].addEventListenner('click', function () {
+        var modalOrcamento = document.querySelector('#jl-modal-orcamento');
+        var overlay = document.querySelector('.jl-overlay');
+        overlay.classList.toggle('jl-is-open');
+        menuMobile.classList.toggle('jl-menu-is-open');
+    });
+}
