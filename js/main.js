ac25cf1e154135dd9ce3c73e7e121bb462ecea8d
Paula Vieira <eipvi@proton.me>
2023-03-12 20:21:25 -0300

    lemon-loader
    12/03/2023

--- js/main.js
+++ js/main.js
@@ -3,6 +3,17 @@ var btnContact = document.querySelector('.jl-btn-contact');
 var toggleMenu = document.querySelectorAll('.jl-toggle-menu');
 var menuMobile = document.querySelector('.jl-menu-mob');
 
+//page preloader
+window.addEventListener('load', function(){
+    var pagePreloder = document.querySelector('.jl-preloader')
+
+    this.setTimeout(function (){
+    pagePreloder.classList.add('jl-fade-out');
+    },1000);
+});
+
+
+//Abrindo e fechando informações de contato
 btnContact.addEventListener('click', function () {
     var boxContact = document.querySelector('.jl-contact-info');
     boxContact.classList.toggle('jl-is-open');
